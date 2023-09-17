f2db668d2ad8318543c8ec2417d4abda6e2cc70f
Kennynet66 <kennynet66@gmail.com>
2023-09-17 18:10:14 +0300

    I'm not going anywhere. You can print that wherever you want to. I'm here and I'm a Spur for life

--- _printf.c
+++ _printf.c
@@ -2,43 +2,70 @@
 #include <stdio.h>
 #include <stdarg.h>
 /**
+ * print_percent - Prints % sign
+ * Return: Always 1 for success
+*/
+int print_percent(void)
+{
+	putchar('%');
+	return (1);
+}
+/**
+ * print_char - Print the character
+ * @character: Takes the character
+ * Return: Always 1
+*/
+int print_char(int character)
+{
+	putchar(character);
+	return (1);
+}
+
+/**
+ * print_string - Simplify the work
+ * @str: This is the pointer
+ * Return: The count always 0
+*/
+int print_string(const char *str)
+{
+	int count = 0;
+
+	while (*str)
+	{
+		putchar(*str);
+		str++;
+		count++;
+	}
+	return (count);
+}
+/**
  * _printf - Custom printf
  * @format: Parsed in parameter
  * Return: the number of characters printed
 */
 int _printf(const char *format, ...)
 {
-	int i, j, count = 0;
-	char character;
-
+	int i, count = 0;
 	va_list args;
 
 	va_start(args, format);
-
 	for (i = 0; format[i] != '\0'; i++)
 	{
 		if (format[i] == '%')
 		{
 			if (format[i + 1] == 'c')
 			{
-				character = va_arg(args, int);
+				char character = va_arg(args, int);
 
-				putchar (character);
 				i++;
-				count++;
-			}
-			else if (format[i + 1] == 's')
+				count += print_char(character);
+			} else if (format[i + 1] == 's')
 			{
 				char *str = va_arg(args, char *);
 
-				for (j = 0; str[j] != '\0'; j++)
-				{
-					putchar(str[j]);
-					count++;
-				}
-					i++;
-			}
-			else if (format[i + 1] == '%')
+				i++;
+				count += print_string(str);
+			} else if (format[i + 1] == '%')
 			{
 				putchar ('%');
 				count++;
