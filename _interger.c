84dfb139a06056276618900ed08396dc311c276d
Kennynet66 <kennynet66@gmail.com>
2023-09-18 01:31:38 +0300

    Error fix

--- _interger.c
+++ _interger.c
@@ -1,3 +1,5 @@
+#include "main.h"
+#include <stdio.h>
 #include <stdarg.h>
 #include <unistd.h>
 /**
@@ -7,33 +9,33 @@
 */
 int _printf(const char *format, ...)
 {
-        va_list args;
-        char f;
-        int i;
+	va_list args;
+	char f;
+	int i;
 
-        va_start(args, format);
-        for (i = 0; format[i] != '\0'; i++)
-        {
-                if (format[i] == '%')
-                {
-                        f = format[i + 1];
-                        if (f == 'd'  || f == 'i')
-                        {
-                                int num = va_arg(args, int);
+	va_start(args, format);
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		if (format[i] == '%')
+		{
+			f = format[i + 1];
+			if (f == 'd'  || f == 'i')
+			{
+				int num = va_arg(args, int);
 
-                                printf("%d", num);
-                                i++;
-                        }
-                        else
-                        {
-                                putchar('%');
-                                putchar(format[i + 1]);
-                                i++;
-                        }
-                } else
-                {
-                        putchar(format[i]);
-                }
-        }
-        va_end(args);
+				printf("%d", num);
+				i++;
+			}
+			else
+			{
+				putchar('%');
+				putchar(format[i + 1]);
+				i++;
+			}
+		} else
+		{
+			putchar(format[i]);
+		}
+	}
+	va_end(args);
 }
