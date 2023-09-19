0a1f484e9c562cd3e1323ef06242154bf30c54e5
obusandy <obungasandrah@gmail.com>
2023-09-19 04:16:11 -0700

    other changes

--- _printf.c
+++ _printf.c
@@ -37,8 +37,6 @@ int _printf(const char *format, ...)
 {
 	int i, count = 0;
 	va_list args;
-	if (!format || (format[0] == '%' && format[1] == '\0'))
-		return (-1);
 
 	va_start(args, format);
 	for (i = 0; format[i] != '\0'; i++)
