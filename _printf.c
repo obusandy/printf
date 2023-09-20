619bcebfdb7a67fd8ffa165681f0f9ffe624f325
Kennynet66 <kennynet66@gmail.com>
2023-09-20 11:38:03 +0300

    Another fixation

--- _printf.c
+++ _printf.c
@@ -3,12 +3,35 @@
 #include <stdarg.h>
 #include <unistd.h>
 /**
- * _printf - Custom printf
- * @format: format string
- * Return: count
+ * _print_string - Handle %s format specifier
+ * @args: va_list containing the argument
+ * @count: Pointer to the count variable
+ */
+void _print_string(va_list args, int *count)
+{
+	char *str = va_arg(args, char *);
+
+	if (str == NULL)
+	{
+		str = "(null)";
+	}
+	while (*str)
+	{
+		*count += _myputchar(*str);
+		str++;
+	}
+}
+/**
+* _printf - Custom printf
+* @format: format string
+* Return: count
 */
 int _printf(const char *format, ...)
 {
+	if (format == NULL)
+	{
+		return (-1);
+	}
 	int i, count = 0;
 	va_list args;
 
@@ -24,17 +47,7 @@ int _printf(const char *format, ...)
 			i++;
 		} else if (format[i + 1] == 's')
 		{
-			char *str = va_arg(args, char *);
-
-			if (str == NULL)
-			{
-				str = "(null)";
-			}
-			while (*str)
-			{
-				count += _myputchar(*str);
-				str++;
-			}
+			_print_string(args, &count);
 			i++;
 		} else if (format[i + 1] == '%')
 		{
